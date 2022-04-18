d0a4a6ffdc09edad22a61682178200b347593178
kir03 <pehenko.k@icloud.com>
2022-04-19 00:30:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Troll
+{
+    internal class Program
+    {
+        static string Clining(string comment)
+        {
+            string vowels = "аёуеэоыяиюЁУЕЭОАЫЯИЮ";
+            string newcom = string.Empty;
+            int count = 0;
+            for (int j = 0; j < comment.Length; j++)
+            {
+                count = 0;
+
+                for (int i = 0; i < vowels.Length; i++)
+                {
+                    if (vowels[i] == comment[j])
+                        count = 1;
+
+                }
+                if (count == 0)
+                    newcom = newcom + comment[j];
+            }
+            return newcom;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Жду комментарий, троль!");
+            Console.WriteLine("А что ты на это скажешь?" + Environment.NewLine + Clining(Console.ReadLine()));
+        }
+    }
+}
